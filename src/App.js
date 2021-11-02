509ec64c8fda444aac5c393f6df867acbe9feab9
Bahaa Ehab El Dien Abdelmawla <mbcompany888@gmail.com>
2021-11-02 11:53:16 +0200

    add truffle and dependiences

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span className = "alert alert-primary">ArDapps</span> Starter Kit For Blockchain Projects with React
         </p>
+     
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://ardapps.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          More dapps Courses
         </a>
       </header>
     </div>
